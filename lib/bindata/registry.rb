630bac8d602b04cb68ee5b54dfd7073d9e876bca
dmendel <dmendel@7cbc0bc6-12c3-4201-99c3-d97d1b75f636>
2008-06-01 09:50:10 +0000

    Exposed #underscore_name method in Registry

--- lib/bindata/registry.rb
+++ lib/bindata/registry.rb
@@ -9,24 +9,29 @@ module BinData
       @registry = {}
     end
 
+    # Convert camelCase +name+ to underscore style.
+    def underscore_name(name)
+      name.to_s.sub(/.*::/, "").
+                gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+                gsub(/([a-z\d])([A-Z])/,'\1_\2').
+                tr("-", "_").
+                downcase
+    end
+
     # Registers the mapping of +name+ to +klass+.  +name+ is converted
     # from camelCase to underscore style.
     # Returns the converted name
     def register(name, klass)
       # convert camelCase name to underscore style
-      underscore_name = name.to_s.sub(/.*::/, "").
-                             gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
-                             gsub(/([a-z\d])([A-Z])/,'\1_\2').
-                             tr("-", "_").
-                             downcase
+      name = underscore_name(name)
 
       # warn if replacing an existing class
-      if $VERBOSE and (existing = @registry[underscore_name])
+      if $VERBOSE and (existing = @registry[name])
         warn "warning: replacing registered class #{existing} with #{klass}"
       end
 
-      @registry[underscore_name] = klass
-      underscore_name.dup
+      @registry[name] = klass
+      name.dup
     end
 
     # Returns the class matching a previously registered +name+.
