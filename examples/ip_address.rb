8b2020b830417cc1e4c526602ee44dc654dfd974
dmendel <dmendel@7cbc0bc6-12c3-4201-99c3-d97d1b75f636>
2009-09-04 11:20:40 +0000

    Add IP address example

---
+++ examples/ip_address.rb
@@ -0,0 +1,23 @@
+require 'bindata'
+
+# A custom type representing an IP address.
+# The underlying binary representation is a sequence of four octets.
+# The human accessible representation is a dotted quad.
+class IPAddr < BinData::Primitive
+  array :octets, :type => :uint8, :initial_length => 4
+
+  def set(val)
+    ints = val.split(/\./).collect { |int| int.to_i }
+    self.octets = ints
+  end
+
+  def get
+    self.octets.collect { |octet| "%d" % octet }.join(".")
+  end
+end
+
+ip = IPAddr.new
+ip.value = "127.0.0.1"
+
+puts "human readable value:  #{ip}"
+puts "binary representation: #{ip.to_binary_s.inspect}"
