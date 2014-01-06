5ea1116ef7f25cce56f1de4d8a1fe20a26176d15
Dion Mendel <dion@lostrealm.com>
2014-01-06 15:58:26 +0800

    Updated prototype Virtual

--- lib/bindata/virtual.rb
+++ lib/bindata/virtual.rb
@@ -1,4 +1,4 @@
-require "bindata/base_primitive"
+require "bindata/base"
 
 module BinData
   # A virtual field is one that is neither read, written nor occupies space.
@@ -17,31 +17,34 @@ module BinData
   #   obj.a #=> "abcde"
   #   obj.c.offset #=> 10
   #
-  class Virtual < BinData::BasePrimitive
+  #   obj = A.read("abcdeABCDE") #=> BinData::ValidityError: assertion failed for obj.c
+  #
+  # == Parameters
+  #
+  # Parameters may be provided at initialisation to control the behaviour of
+  # an object.  These params include those for BinData::Base as well as:
+  #
+  # [<tt>:assert</tt>]    Raise an error when reading if the value of this
+  #                       evaluated parameter is false.
+  #
+  class Virtual < BinData::Base
 
-    default_parameter :onlyif => false
+    optional_parameter :assert
 
-    class << self
-      def sanitize_parameters!(params) #:nodoc:
-        if params.has_parameter?(:asserted_value)
-          fail ":asserted_value can not be used on virtual field"
-        end
+    def clear?; true; end
+    def assign(val); end
+    def snapshot; nil; end
+    def do_num_bytes; 0; end
+    def do_write(io); end
+
+    def do_read(io)
+      assert!
+    end
+
+    def assert!
+      if has_parameter?(:assert) and not eval_parameter(:assert)
+        raise ValidityError, "assertion failed for #{debug_name}"
       end
-    end
-
-    #---------------
-    private
-
-    def value_to_binary_string(val)
-      ""
-    end
-
-    def read_and_return_value(io)
-      ""
-    end
-
-    def sensible_default
-      ""
     end
   end
 end
