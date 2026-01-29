2837e74ce926fc9ec68413c16c813016b11de355
Dion Mendel <bindata@dmau.org>
2026-01-29 15:18:09 +0800

    Registry now supports multiple namespaces

--- lib/bindata/registry.rb
+++ lib/bindata/registry.rb
@@ -7,36 +7,43 @@ module BinData
   # Numerics (integers and floating point numbers) have an endian property as
   # part of their name (e.g. int32be, float_le).
   #
+  # Classes exist in a namespace that mirrors the Ruby module hierarchy.
+  #
   # Classes can be looked up based on their full name or an abbreviated +name+
   # with +hints+.
   #
   # There are two hints supported, :endian and :search_prefix.
   #
-  #   #lookup("int32", { endian: :big }) will return Int32Be.
+  #   #lookup("", "int32", { endian: :big }) will return Int32Be.
   #
-  #   #lookup("my_type", { search_prefix: :ns }) will return NsMyType.
+  #   #lookup("", "my_type", { search_prefix: :ns }) will return Ns::MyType.
   #
   # Names are stored in under_score_style, not camelCase.
   class Registry
     def initialize
       @registry = {}
+      @backwards_compatible_registry = {}
     end
 
-    def register(name, class_to_register)
-      return if name.nil? || class_to_register.nil?
+    def register(namespace, name, class_to_register)
+      return if namespace.nil? || name.nil? || class_to_register.nil?
 
-      formatted_name = underscore_name(name)
-      warn_if_name_is_already_registered(formatted_name, class_to_register)
+      search = name_with_prefix(name, namespace)
+      warn_if_name_is_already_registered(search, class_to_register)
 
-      @registry[formatted_name] = class_to_register
+      @registry[search] = class_to_register
+      @backwards_compatible_registry[underscore_name(name)] = search
     end
 
-    def unregister(name)
-      @registry.delete(underscore_name(name))
+    def unregister(namespace, name)
+      search = name_with_prefix(name, namespace)
+
+      @registry.delete(search)
+      @backwards_compatible_registry.delete(underscore_name(name))
     end
 
-    def lookup(name, hints = {})
-      search_names(name, hints).each do |search|
+    def lookup(namespace, name, hints = {})
+      search_names(namespace, name, hints).each do |search|
         register_dynamic_class(search)
         if @registry.has_key?(search)
           return @registry[search]
@@ -44,7 +51,7 @@ module BinData
       end
 
       # give the user a hint if the endian keyword is missing
-      search_names(name, hints.merge(endian: :big)).each do |search|
+      search_names(namespace, name, hints.merge(endian: :big)).each do |search|
         register_dynamic_class(search)
         if @registry.has_key?(search)
           raise(UnRegisteredTypeError, "#{name}, do you need to specify endian?")
@@ -68,24 +75,62 @@ module BinData
     #---------------
     private
 
-    def search_names(name, hints)
-      base = underscore_name(name)
+    def search_names(namespace, name, hints)
+      searches = backwards_compatible_search_names(name, hints)
+      return searches unless searches.empty?
+
       searches = []
 
-      search_prefix = [""] + Array(hints[:search_prefix])
+      # prioritise BinData classes
+      nwp = name_with_prefix(name, "BinData")
+      nwe = name_with_endian(nwp, hints[:endian])
+      searches << nwp
+      searches << nwe if nwe
+
+      ns = underscore_name(namespace)
+      loop do
+        search_prefix = [""] + Array(hints[:search_prefix])
+        search_prefix.each do |prefix|
+          nwp = name_with_prefix(name, name_with_prefix(prefix, ns))
+          nwe = name_with_endian(nwp, hints[:endian])
+
+          searches << nwp
+          searches << nwe if nwe
+        end
+
+        break if ns == ""
+        ns.sub!(/(?:^|_)[^_]*$/, "")
+      end
+
+      searches
+    end
+
+    # The old way of providing namespaces was to prefix the class name.
+    # Here we provide the backward compatibility for existing code.
+    def backwards_compatible_search_names(name, hints)
+      return [] unless hints.has_key?(:search_prefix)
+
+      searches = []
+      search_prefix = Array(hints[:search_prefix])
       search_prefix.each do |prefix|
-        nwp = name_with_prefix(base, prefix)
+        nwp = name_with_prefix(name, prefix)
         nwe = name_with_endian(nwp, hints[:endian])
 
-        searches << nwp
-        searches << nwe if nwe
+        found_search = @backwards_compatible_registry[nwp]
+        searches << found_search if found_search
+
+        if nwe
+          found_search = @backwards_compatible_registry[nwe]
+          searches << found_search if found_search
+        end
       end
 
       searches
     end
 
     def name_with_prefix(name, prefix)
-      prefix = prefix.to_s.chomp('_')
+      name = underscore_name(name)
+      prefix = underscore_name(prefix).chomp('_')
       if prefix == ""
         name
       else
@@ -97,7 +142,7 @@ module BinData
       return nil if endian.nil?
 
       suffix = (endian == :little) ? 'le' : 'be'
-      if /^u?int\d+$/.match?(name)
+      if /^bin_data_u?int\d+$/.match?(name)
         name + suffix
       else
         name + '_' + suffix
@@ -105,8 +150,11 @@ module BinData
     end
 
     def register_dynamic_class(name)
-      if /^u?int\d+(le|be)$/.match?(name) || /^s?bit\d+(le)?$/.match?(name)
-        class_name = name.gsub(/(?:^|_)(.)/) { $1.upcase }
+      return unless /^bin_data_/ =~ name
+
+      if /^bin_data_u?int\d+(le|be)$/.match?(name) || /^bin_data_s?bit\d+(le)?$/.match?(name)
+        base_name = name.sub(/^bin_data_/, "")
+        class_name = base_name.gsub(/(?:^|_)(.)/) { $1.upcase }
         begin
           # call const_get for side effect of creating class
           BinData.const_get(class_name)
--- test/registry_test.rb
+++ test/registry_test.rb
@@ -7,31 +7,32 @@ describe BinData::Registry do
   B = Class.new
   C = Class.new
   D = Class.new
+  E = Class.new
 
   let(:r) { BinData::Registry.new }
 
   it "lookups registered names" do
-    r.register('ASubClass', A)
-    r.register('AnotherSubClass', B)
+    r.register("", 'ASubClass', A)
+    r.register("", 'AnotherSubClass', B)
 
-    _(r.lookup('ASubClass')).must_equal A
-    _(r.lookup('a_sub_class')).must_equal A
-    _(r.lookup('AnotherSubClass')).must_equal B
-    _(r.lookup('another_sub_class')).must_equal B
+    _(r.lookup("", 'ASubClass')).must_equal A
+    _(r.lookup("", 'a_sub_class')).must_equal A
+    _(r.lookup("", 'AnotherSubClass')).must_equal B
+    _(r.lookup("", 'another_sub_class')).must_equal B
   end
 
   it "does not lookup unregistered names" do
     _ {
-      r.lookup('a_non_existent_sub_class')
+      r.lookup("", 'a_non_existent_sub_class')
     }.must_raise BinData::UnRegisteredTypeError
   end
 
   it "unregisters names" do
-    r.register('ASubClass', A)
-    r.unregister('ASubClass')
+    r.register("", 'ASubClass', A)
+    r.unregister("", 'ASubClass')
 
     _ {
-      r.lookup('ASubClass')
+      r.lookup("", 'ASubClass')
     }.must_raise BinData::UnRegisteredTypeError
   end
 
@@ -39,10 +40,10 @@ describe BinData::Registry do
     w, $-w = $-w, nil  # disable warning
 
     begin
-      r.register('A', A)
-      r.register('A', B)
+      r.register("", 'A', A)
+      r.register("", 'A', B)
 
-      _(r.lookup('a')).must_equal B
+      _(r.lookup("", 'a')).must_equal B
     ensure
       $-w = w
     end
@@ -65,21 +66,21 @@ describe BinData::Registry, "with numerics" do
   let(:r) { BinData::RegisteredClasses }
 
   it "lookup integers with endian" do
-    _(r.lookup("int24", {endian: :big}).to_s).must_equal "BinData::Int24be"
-    _(r.lookup("int24", {endian: :little}).to_s).must_equal "BinData::Int24le"
-    _(r.lookup("uint24", {endian: :big}).to_s).must_equal "BinData::Uint24be"
-    _(r.lookup("uint24", {endian: :little}).to_s).must_equal "BinData::Uint24le"
+    _(r.lookup("", 'int24', {endian: :big}).to_s).must_equal "BinData::Int24be"
+    _(r.lookup("", 'int24', {endian: :little}).to_s).must_equal "BinData::Int24le"
+    _(r.lookup("xx", 'uint24', {endian: :big}).to_s).must_equal "BinData::Uint24be"
+    _(r.lookup("yy", 'uint24', {endian: :little}).to_s).must_equal "BinData::Uint24le"
   end
 
   it "does not lookup integers without endian" do
     _ {
-      r.lookup("int24")
+      r.lookup("", 'int24')
     }.must_raise BinData::UnRegisteredTypeError
   end
 
   it "provides a nice error message when endian is omitted" do
     begin
-      r.lookup("int24")
+      r.lookup("", 'int24')
     rescue BinData::UnRegisteredTypeError => e
       _(e.message).must_equal "int24, do you need to specify endian?"
     end
@@ -87,41 +88,41 @@ describe BinData::Registry, "with numerics" do
 
   it "does not lookup non byte based integers" do
     _ {
-      r.lookup("int3")
+      r.lookup("", 'int3')
     }.must_raise BinData::UnRegisteredTypeError
     _ {
-      r.lookup("int3", {endian: :big})
+      r.lookup("", 'int3', {endian: :big})
     }.must_raise BinData::UnRegisteredTypeError
     _ {
-      r.lookup("int3", {endian: :little})
+      r.lookup("", 'int3', {endian: :little})
     }.must_raise BinData::UnRegisteredTypeError
   end
 
   it "lookup floats with endian" do
-    _(r.lookup("float", {endian: :big}).to_s).must_equal "BinData::FloatBe"
-    _(r.lookup("float", {endian: :little}).to_s).must_equal "BinData::FloatLe"
-    _(r.lookup("double", {endian: :big}).to_s).must_equal "BinData::DoubleBe"
-    _(r.lookup("double", {endian: :little}).to_s).must_equal "BinData::DoubleLe"
+    _(r.lookup("", 'float', {endian: :big}).to_s).must_equal "BinData::FloatBe"
+    _(r.lookup("", 'float', {endian: :little}).to_s).must_equal "BinData::FloatLe"
+    _(r.lookup("xx", 'double', {endian: :big}).to_s).must_equal "BinData::DoubleBe"
+    _(r.lookup("yy", 'double', {endian: :little}).to_s).must_equal "BinData::DoubleLe"
   end
 
   it "lookup bits" do
-    _(r.lookup("bit5").to_s).must_equal "BinData::Bit5"
-    _(r.lookup("sbit5").to_s).must_equal "BinData::Sbit5"
-    _(r.lookup("bit6le").to_s).must_equal "BinData::Bit6le"
+    _(r.lookup("", 'bit5').to_s).must_equal "BinData::Bit5"
+    _(r.lookup("", 'sbit5').to_s).must_equal "BinData::Sbit5"
+    _(r.lookup("", 'bit6le').to_s).must_equal "BinData::Bit6le"
   end
 
   it "lookup bits by ignoring endian" do
-    _(r.lookup("bit2", {endian: :big}).to_s).must_equal "BinData::Bit2"
-    _(r.lookup("bit3le", {endian: :big}).to_s).must_equal "BinData::Bit3le"
-    _(r.lookup("bit2", {endian: :little}).to_s).must_equal "BinData::Bit2"
-    _(r.lookup("bit3le", {endian: :little}).to_s).must_equal "BinData::Bit3le"
+    _(r.lookup("", 'bit2', {endian: :big}).to_s).must_equal "BinData::Bit2"
+    _(r.lookup("", 'bit3le', {endian: :big}).to_s).must_equal "BinData::Bit3le"
+    _(r.lookup("", 'bit2', {endian: :little}).to_s).must_equal "BinData::Bit2"
+    _(r.lookup("", 'bit3le', {endian: :little}).to_s).must_equal "BinData::Bit3le"
   end
 
   it "lookup signed bits by ignoring endian" do
-    _(r.lookup("sbit2", {endian: :big}).to_s).must_equal "BinData::Sbit2"
-    _(r.lookup("sbit3le", {endian: :big}).to_s).must_equal "BinData::Sbit3le"
-    _(r.lookup("sbit2", {endian: :little}).to_s).must_equal "BinData::Sbit2"
-    _(r.lookup("sbit3le", {endian: :little}).to_s).must_equal "BinData::Sbit3le"
+    _(r.lookup("", 'sbit2', {endian: :big}).to_s).must_equal "BinData::Sbit2"
+    _(r.lookup("", 'sbit3le', {endian: :big}).to_s).must_equal "BinData::Sbit3le"
+    _(r.lookup("", 'sbit2', {endian: :little}).to_s).must_equal "BinData::Sbit2"
+    _(r.lookup("", 'sbit3le', {endian: :little}).to_s).must_equal "BinData::Sbit3le"
   end
 end
 
@@ -129,29 +130,29 @@ describe BinData::Registry, "with endian specific types" do
   let(:r) { BinData::Registry.new }
 
   before do
-    r.register('a_le', A)
-    r.register('b_be', B)
+    r.register("", 'a_le', A)
+    r.register("", 'b_be', B)
   end
   
   it "lookup little endian types" do
-    _(r.lookup('a', {endian: :little})).must_equal A
+    _(r.lookup("", 'a', {endian: :little})).must_equal A
   end
 
   it "lookup big endian types" do
-    _(r.lookup('b', {endian: :big})).must_equal B
+    _(r.lookup("", 'b', {endian: :big})).must_equal B
   end
 
   it "does not lookup types with non existent endian" do
     _ {
-      r.lookup('a', {endian: :big})
+      r.lookup("", 'a', {endian: :big})
     }.must_raise BinData::UnRegisteredTypeError
   end
 
   it "lookup prefers exact type" do
-    r.register('c', C)
-    r.register('c_le', D)
+    r.register("", 'c', C)
+    r.register("", 'c_le', D)
 
-    _(r.lookup('c', {endian: :little})).must_equal C
+    _(r.lookup("", 'c', {endian: :little})).must_equal C
   end
 end
 
@@ -159,23 +160,79 @@ describe BinData::Registry, "with search_prefix" do
   let(:r) { BinData::Registry.new }
 
   before do
-    r.register('a_f', A)
-    r.register('b_f', B)
+    r.register("", 'a_f', A)
+    r.register("", 'b_f', B)
   end
 
   it "lookup single search_prefix" do
-    _(r.lookup('f', {search_prefix: :a})).must_equal A
+    _(r.lookup("", 'f', {search_prefix: :a})).must_equal A
   end
 
   it "lookup single search_prefix with endian" do
-    _(r.lookup('f', {search_prefix: :a, endian: :little})).must_equal A
+    _(r.lookup("", 'f', {search_prefix: :a, endian: :little})).must_equal A
   end
 
   it "lookup multiple search_prefix" do
-    _(r.lookup('f', {search_prefix: [:x, :a]})).must_equal A
+    _(r.lookup("", 'f', {search_prefix: [:x, :a]})).must_equal A
   end
 
   it "lookup first match in search_prefix" do
-    _(r.lookup('f', {search_prefix: [:a, :b]})).must_equal A
+    _(r.lookup("", 'f', {search_prefix: [:a, :b]})).must_equal A
+  end
+end
+
+describe BinData::Registry, "with namespaces" do
+  let(:r) { BinData::Registry.new }
+
+  before do
+    r.register('mod_a_mod_b', 'obj1', A)      # ModA::ModB::Obj1
+    r.register('mod_a_mod_b', 'obj2', B)      # ModA::ModB::Obj2
+    r.register('mod_a_mod_b', 'ns1_obj3', C)  # ModA::ModB::Ns1Obj3
+    r.register('mod_a_mod_c', 'obj1', D)      # ModA::ModC::Obj1
+    r.register('mod_a_mod_c', 'obj4', E)      # ModA::ModC::Obj4
+  end
+
+  it "lookups inside the namespace" do
+    _(r.lookup("mod_a_mod_c", 'obj1')).must_equal D
+    _(r.lookup("mod_a_mod_c", 'obj4')).must_equal E
+  end
+
+  it "doesn't lookup inside a different namespace" do
+    _ {
+      r.lookup("mod_a_mod_c", 'obj2')
+    }.must_raise BinData::UnRegisteredTypeError
+
+    _ {
+      r.lookup("mod_a", 'obj2')
+    }.must_raise BinData::UnRegisteredTypeError
+
+    _ {
+      r.lookup("", 'obj2')
+    }.must_raise BinData::UnRegisteredTypeError
+  end
+
+  it "lookups with relative namespace" do
+    _(r.lookup("mod_a_mod_c", 'mod_b_obj1')).must_equal A
+    _(r.lookup("mod_a", 'mod_b_obj1')).must_equal A
+  end
+
+  it "lookups with absolute namespace" do
+    _(r.lookup("mod_a_mod_c", 'mod_a_mod_b_obj1')).must_equal A
+    _(r.lookup("", 'mod_a_mod_b_obj1')).must_equal A
+  end
+
+  it "lookups with search_prefix" do
+    _(r.lookup("mod_a_mod_c", 'obj1', {search_prefix: :mod_b})).must_equal D
+    _(r.lookup("mod_a", 'obj1', {search_prefix: :mod_b})).must_equal A
+    _(r.lookup("mod_a_mod_c", 'obj2', {search_prefix: :mod_b})).must_equal B
+    _(r.lookup("mod_a_mod_c", 'obj2', {search_prefix: :mod_a_mod_b})).must_equal B
+    _(r.lookup("", 'obj1', {search_prefix: [:mod_a_mod_b, :mod_a_mod_c]})).must_equal A
+    _(r.lookup("", 'obj1', {search_prefix: [:mod_a_mod_c, :mod_a_mod_b]})).must_equal D
+  end
+
+  it "lookups with old style search_prefix" do
+    _(r.lookup("mod_a_mod_b", 'obj3', {search_prefix: :ns1})).must_equal C
+    _(r.lookup("mod_a_mod_c", 'obj3', {search_prefix: :ns1})).must_equal C
+    _(r.lookup("", 'obj3', {search_prefix: :ns1})).must_equal C
   end
 end
